85e471744cd4db102b2c99a54ef1dbe6f3d0195b
robertLichtnow <matheusrobert8@gmail.com>
2019-02-26 20:23:18 -0300

    Criado fluxo para criação dos inputs

--- app/src/main/java/com/lichtnow/robert/wekaopener/ClassifierActivity.java
+++ app/src/main/java/com/lichtnow/robert/wekaopener/ClassifierActivity.java
@@ -1,26 +1,21 @@
 package com.lichtnow.robert.wekaopener;
 
-import android.app.ActionBar;
-import android.content.Context;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.WindowManager;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import udc.edu.weka.attribute.Attribute;
@@ -58,17 +53,40 @@ public class ClassifierActivity extends AppCompatActivity {
 
         List<Attribute> atributos = this.cls.getAttributes();
         for(int i=0;i<atributos.size();i++){
-                //Fluxo para numéricos
+            TextView tv = new TextView(this);
+            tv.setWidth(this.camposList.getWidth());
+            tv.setMaxLines(1);
+            tv.setEllipsize(TextUtils.TruncateAt.END);
+            tv.setText(atributos.get(i).getName());
+            tv.setTextSize(17);
+            LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+            llp.setMargins(0,i == 0? 0 : 15,0,0);
+            tv.setLayoutParams(llp);
+            this.camposList.addView(tv);
+
+            if(atributos.get(i).getAttributeType().equals(AttributeType.NOMINAL)){
+                Spinner s = new Spinner(this);
+                s.setMinimumWidth(this.camposList.getWidth());
+                ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, atributos.get(i).getPossibleValues());
+                s.setAdapter(spinnerArrayAdapter);
+                s.setBackgroundResource(R.drawable.spinner_background);
+                this.camposList.addView(s);
+            }
+            else {
                 EditText et = new EditText(this);
                 et.setWidth(this.camposList.getWidth());
                 et.setRawInputType(InputType.TYPE_CLASS_NUMBER);
-                tw = new TextWatcher(){
-                    public void afterTextChanged(Editable s) {}
-                    public void beforeTextChanged(CharSequence s, int start,int count, int after) {}
+                et.setText("0");
+                tw = new TextWatcher() {
+                    public void afterTextChanged(Editable s) {
+                    }
+
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    }
 
                     public void onTextChanged(CharSequence s, int start,
                                               int before, int count) {
-                        if(s.length() > 0 && !s.toString().equals("-")) {
+                        if (s.length() > 0 && !s.toString().equals("-")) {
                             StringBuilder sb = new StringBuilder(s.length());
                             sb.append(s);
                             try {
@@ -82,9 +100,7 @@ public class ClassifierActivity extends AppCompatActivity {
                 };
                 et.addTextChangedListener(tw);
                 this.camposList.addView(et);
-
-
-
+            }
         }
     }
 
