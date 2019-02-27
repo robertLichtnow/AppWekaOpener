42b60e71012a2a7e9a1a7e4de527518b0f0571d0
robertLichtnow <matheusrobert8@gmail.com>
2019-02-26 22:19:15 -0300

    Iniciado fluxo de classificação

--- app/src/main/java/com/lichtnow/robert/wekaopener/ClassifierActivity.java
+++ app/src/main/java/com/lichtnow/robert/wekaopener/ClassifierActivity.java
@@ -8,7 +8,6 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -16,10 +15,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import udc.edu.weka.attribute.Attribute;
 import udc.edu.weka.attribute.AttributeType;
+import udc.edu.weka.attribute.DenseAttribute;
+import udc.edu.weka.attribute.DenseInstance;
 import udc.edu.weka.classifiers.Classifier;
 
 public class ClassifierActivity extends AppCompatActivity {
@@ -53,53 +55,54 @@ public class ClassifierActivity extends AppCompatActivity {
 
         List<Attribute> atributos = this.cls.getAttributes();
         for(int i=0;i<atributos.size();i++){
-            TextView tv = new TextView(this);
-            tv.setWidth(this.camposList.getWidth());
-            tv.setMaxLines(1);
-            tv.setEllipsize(TextUtils.TruncateAt.END);
-            tv.setText(atributos.get(i).getName());
-            tv.setTextSize(17);
-            LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-            llp.setMargins(0,i == 0? 0 : 15,0,0);
-            tv.setLayoutParams(llp);
-            this.camposList.addView(tv);
+            if(cls.getClassIndex() != i) {
+                TextView tv = new TextView(this);
+                tv.setWidth(this.camposList.getWidth());
+                tv.setMaxLines(1);
+                tv.setEllipsize(TextUtils.TruncateAt.END);
+                tv.setText(atributos.get(i).getName());
+                tv.setTextSize(17);
+                LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                llp.setMargins(0, i == 0 ? 0 : 15, 0, 0);
+                tv.setLayoutParams(llp);
+                this.camposList.addView(tv);
 
-            if(atributos.get(i).getAttributeType().equals(AttributeType.NOMINAL)){
-                Spinner s = new Spinner(this);
-                s.setMinimumWidth(this.camposList.getWidth());
-                ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, atributos.get(i).getPossibleValues());
-                s.setAdapter(spinnerArrayAdapter);
-                s.setBackgroundResource(R.drawable.spinner_background);
-                this.camposList.addView(s);
-            }
-            else {
-                EditText et = new EditText(this);
-                et.setWidth(this.camposList.getWidth());
-                et.setRawInputType(InputType.TYPE_CLASS_NUMBER);
-                et.setText("0");
-                tw = new TextWatcher() {
-                    public void afterTextChanged(Editable s) {
-                    }
+                if (atributos.get(i).getAttributeType().equals(AttributeType.NOMINAL)) {
+                    Spinner s = new Spinner(this);
+                    s.setMinimumWidth(this.camposList.getWidth());
+                    ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, atributos.get(i).getPossibleValues());
+                    s.setAdapter(spinnerArrayAdapter);
+                    s.setBackgroundResource(R.drawable.spinner_background);
+                    this.camposList.addView(s);
+                } else {
+                    EditText et = new EditText(this);
+                    et.setWidth(this.camposList.getWidth());
+                    et.setRawInputType(InputType.TYPE_CLASS_NUMBER);
+                    et.setText("0");
+                    tw = new TextWatcher() {
+                        public void afterTextChanged(Editable s) {
+                        }
 
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                    }
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        }
 
-                    public void onTextChanged(CharSequence s, int start,
-                                              int before, int count) {
-                        if (s.length() > 0 && !s.toString().equals("-")) {
-                            StringBuilder sb = new StringBuilder(s.length());
-                            sb.append(s);
-                            try {
-                                Double.parseDouble(sb.toString());
-                            } catch (NumberFormatException ex) {
-                                et.setText(s.toString().substring(0, s.length() - 1));
-                                et.setSelection(et.getText().length());
+                        public void onTextChanged(CharSequence s, int start,
+                                                  int before, int count) {
+                            if (s.length() > 0 && !s.toString().equals("-")) {
+                                StringBuilder sb = new StringBuilder(s.length());
+                                sb.append(s);
+                                try {
+                                    Double.parseDouble(sb.toString());
+                                } catch (NumberFormatException ex) {
+                                    et.setText(s.toString().substring(0, s.length() - 1));
+                                    et.setSelection(et.getText().length());
+                                }
                             }
                         }
-                    }
-                };
-                et.addTextChangedListener(tw);
-                this.camposList.addView(et);
+                    };
+                    et.addTextChangedListener(tw);
+                    this.camposList.addView(et);
+                }
             }
         }
     }
@@ -116,8 +119,32 @@ public class ClassifierActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
             case R.id.send_btn:
-                Toast.makeText(ClassifierActivity.this, "Clicou para classificar", Toast.LENGTH_SHORT).show();
-                //TODO aqui vem a lógica de classificar instância
+                int index = 0;
+                try{
+                    List<DenseAttribute> attrs = new ArrayList<>();
+                    for(int i=0; i<this.camposList.getChildCount();i++){
+                        if(this.camposList.getChildAt(i).getClass() == Spinner.class){
+                            DenseAttribute attr = new DenseAttribute();
+                            attr.setType(AttributeType.NOMINAL);
+                            Spinner s = (Spinner)this.camposList.getChildAt(i);
+                            attr.setNominalValue(s.getSelectedItem().toString());
+                            attrs.add(attr);
+                        }else if(this.camposList.getChildAt(i).getClass() == EditText.class){
+                            DenseAttribute attr = new DenseAttribute();
+                            attr.setType(AttributeType.NUMERIC);
+                            EditText et = (EditText)this.camposList.getChildAt(i);
+                            double valor = Double.parseDouble(et.getText().toString());
+                            attr.setNumericalValue(valor);
+                            attrs.add(attr);
+                        }
+                    }
+                    DenseInstance ins = new DenseInstance(1,(DenseAttribute[])attrs.toArray(new DenseAttribute[attrs.size()]),this.cls);
+                    DenseAttribute classe = this.cls.classifyInstance(ins);
+                    //TODO ao invés de um TOAST, mandar para outra página para visualização dos resultados
+                    Toast.makeText(ClassifierActivity.this, classe.getNominalValue(), Toast.LENGTH_SHORT).show();
+                }catch(Exception ex){
+                    Toast.makeText(ClassifierActivity.this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+                }
                 return true;
             default: return super.onOptionsItemSelected(item);
         }
